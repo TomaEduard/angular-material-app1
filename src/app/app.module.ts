69cd28ec21d74e5806ef477eb861f6a9a58132d0
eduard.daniel.toma <eduard.daniel.toma@adicomsoft.ro>
2020-07-09 12:25:58 +0300

    test some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule } from '@angular/material/button'
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatStepperModule } from '@angular/material/stepper';
 
 @NgModule({
   declarations: [
@@ -17,6 +28,17 @@ import { MatButtonModule } from '@angular/material/button'
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatButtonToggleModule,
+    MatBadgeModule,
+    MatChipsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatListModule,
+    MatStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
